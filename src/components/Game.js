54c1cb72ef446e09a2b5e93e6ae0c57f174f9da3
Yaniv Gabay <yanivga@std.hac.ac.il>
2024-05-20 20:38:59 +0300

    moved the game over instead of a state to a static consts

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import Card from './Card';
 import { initializeDeck } from '../Utilities';
@@ -11,35 +11,35 @@ import { isEveryCardMatched } from '../Utilities';
 function Game() {
   const location = useLocation();
   const navigate = useNavigate();
-  const { name,rows, cols, flipDelay = 1000 } = location.state?.gameSettings || {name:'default', rows: 4, cols: 4, flipDelay: 1000 };  
+  const { name, rows, cols, flipDelay = 1000 } = location.state?.gameSettings || { name: 'default', rows: 4, cols: 4, flipDelay: 1000 };
   //init deck returns a shuffled cards
   const [cards, setCards] = useState(initializeDeck(rows, cols));
   const [canFlip, setCanFlip] = useState(true);
   const [score, setScore] = useState(0);
-  const [gameOver, setGameOver] = useState(false);
+
   const { addScore } = useHighScores();
-  
 
- 
+  const gameOver = isEveryCardMatched(cards);
 
-  
-    const handleGameComplete = () => {
-      
-        addScore({ name: name, score: score }); // Ensure this function doesn't add duplicates
-        setGameOver(true); // Mark the game as over
-        setTimeout(() => {
-          navigate('/highscores');
-        }, 2000); // Navigate after a delay to show the game over screen or a message
-     
-    };
+
+
+  const handleGameComplete = () => {
+
+    addScore({ name: name, score: score }); // Ensure this function doesn't add duplicates
+    
+    setTimeout(() => {
+      navigate('/highscores');
+    }, 2000); // Navigate after a delay to show the game over screen or a message
+
+  };
 
   const handleCardClick = index => {
     if (!canFlip || cards[index].isFlipped || cards[index].isMatched) return;
     const newCards = cards.map((card, idx) => idx === index ? { ...card, isFlipped: true } : card);
     setCards(newCards);
     processCardMatch(newCards);
-    
-    
+
+
   };
 
   const processCardMatch = (newCards) => {
@@ -55,9 +55,10 @@ function Game() {
   const matchCards = (flippedCards) => {
     if (flippedCards[0].id === flippedCards[1].id) {
       setScore(prevScore => prevScore + 1);
-      const matchedCards = cards.map(card => card.id === flippedCards[0].id ? { ...card, isMatched: true , isFlipped: true } : card);
+      const matchedCards = cards.map(card => card.id === flippedCards[0].id ? { ...card, isMatched: true, isFlipped: true } : card);
       setCards(matchedCards);
-      if(isEveryCardMatched(matchedCards)) {
+      
+      if (isEveryCardMatched(matchedCards)) {
         handleGameComplete();
       }
       setCanFlip(true);
@@ -67,9 +68,9 @@ function Game() {
     }
   };
 
-  
-  console.log(rows, cols, flipDelay);
-  console.log(cards);
+
+  //console.log(rows, cols, flipDelay);
+  //console.log(cards);
 
 
   return (
@@ -80,7 +81,7 @@ function Game() {
         </Grid>
       ))}
 
-     {gameOver && <GameFinished score={score} />}
+      {gameOver && <GameFinished score={score} />}
     </Grid>
   );
 }
